37f6b0298377db45dd25030ae450cd5775d4ddbc
dvirturg <dvirt11@gmail.com>
2025-12-12 16:25:39 +0200

    initialize project with package.json, index.js and mvc structure

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGODB_URI)
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", ()=> {
+    console.log("Connected to MongoDB");
+});
+
+app.use(express.json());
+
+
+// Define Routes
+
+
+app.listen(port, ()=>{
+    console.log(`Example app listening at http://localhost:${port}`);
+});
